b87338207a2703018d4c64cfaa62844e2724c0f8
herrlestrate <vemakus@gmail.com>
2019-12-05 22:18:31 +0300

    Some fixes with md5 hash and using sessions

--- src/main/java/Routers/LoginLink.java
+++ src/main/java/Routers/LoginLink.java
@@ -1,7 +1,16 @@
 package Routers;
 
 import Utils.HttpParser;
+import Utils.Session;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
 
 public class LoginLink {
 
@@ -9,16 +18,38 @@ public class LoginLink {
         String login = parser.getParam("login");
         String password = parser.getParam("password");
 
+        System.out.println(login + " " + password);
+
         JSONObject jsonObject = new JSONObject();
-        if(login.equalsIgnoreCase("root"))
-            jsonObject.put("Validation", "true");
-        else {
-            jsonObject.put("Validation", "false");
-            jsonObject.put("Reason", "Non-root user");
+
+        password = inMD5(password);
+        System.out.println(password);
+
+        ResultSet rs = Router.getMySQLWorker().doLogin(login, password);
+
+        try {
+            if (rs == null || !rs.next()) {
+                jsonObject.put("result", "No such user or incorrect password!");
+                Router.setAnswer(jsonObject);
+                return;
+            }
+
+            Session session = new Session(login);
+            session.setSession(Session.generateSession());
+
+            jsonObject.put("result", "OK");
+            jsonObject.put("session", session.getSession());
+        } catch (SQLException ex){
+            ex.printStackTrace();
+            //Bad login
+            jsonObject.put("result", "No such user or incorrect password!");
         }
 
         Router.setAnswer(jsonObject);
+    }
 
-        //TODO mysql login
+    public static String inMD5(String st){
+
+        return DigestUtils.md5Hex(st);
     }
 }
